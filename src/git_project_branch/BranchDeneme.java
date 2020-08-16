78e56d1c1ff2b0b7fe38e75c10714da6fc15605c
yasemin <muazunes@gmail.com>
2020-08-16 18:38:55 +0200

    fature'dan commit

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -4,6 +4,7 @@ public class BranchDeneme {
 
 	public static void main(String[] args) {
 	System.out.println("Saat 16:28 - master branch ilk commit");
+	System.out.println("18:36- feature branch'den ilk commit");
 	}
 
 }
